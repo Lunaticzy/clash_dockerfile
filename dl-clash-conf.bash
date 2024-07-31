ab45e1fabcdf72318ba164c1e929204df1a5d09f
Yin Zhao <lunaticzy9527@qq.com>
2024-08-01 01:29:11 +0800

    添加回误删的修改管理面板相关代码

--- dl-clash-conf.bash
+++ dl-clash-conf.bash
@@ -10,6 +10,12 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
+if [[ ! -z "$DEFAULT_BACKEND" ]]
+then
+  echo "修改管理面板默认后端..."
+  sed -i "s|http://127.0.0.1:9090|$DEFAULT_BACKEND|g" /root/ui/assets/Setup-*.js
+fi
+
 # 调用 Ruby 脚本来更新配置文件
 update_config.rb "$ConfFile"
 
